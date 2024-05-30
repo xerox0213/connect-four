cf14410262026bd1a3a084cc8b600beb9651695c
xerox0213 <nasreddine021@gmail.com>
2024-05-30 22:38:00 +0200

    feat(Board): création de la méthode isValid
    Permet aux clients de la classe (la facade dans ce cas là) de vérifier si le coup d'un joueur est un joueur valide

--- src/main/java/game/model/Board.java
+++ src/main/java/game/model/Board.java
@@ -13,4 +13,28 @@ public class Board {
         rows = boardSize.getRows();
         tokens = new Token[cols][rows];
     }
+
+    /**
+     * Checks if a move is valid by verifying the column index is within the board's bounds
+     * and the column is not already filled.
+     *
+     * @param colIndex the index of the column to check
+     * @return true if the move is valid, false otherwise
+     */
+    public boolean isMoveValid(int colIndex) {
+        return isOutsideBoard(colIndex) && !isColumnFilled(colIndex);
+    }
+
+    private boolean isOutsideBoard(int colIndex) {
+        return colIndex < 0 || colIndex >= cols;
+    }
+
+    private boolean isColumnFilled(int colIndex) {
+        int rowIndex = 0;
+        while (rowIndex < rows) {
+            if (tokens[colIndex][rowIndex] == null) return false;
+            rowIndex++;
+        }
+        return true;
+    }
 }
