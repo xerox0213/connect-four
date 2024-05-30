ab79599736df78b32e348f6173602f28f19c5535
xerox0213 <nasreddine021@gmail.com>
2024-05-30 22:11:33 +0200

    feat: création de l'énum BoardSize
    Permet de représenter les différentes tailles possibles du plateau de jeu

---
+++ src/main/java/game/model/BoardSize.java
@@ -0,0 +1,12 @@
+package game.model;
+
+/**
+ * Represents the possible board sizes for the Connect Four game.
+ */
+public enum BoardSize {
+    SEVEN_BY_SIX,
+    EIGHT_BY_SEVEN,
+    EIGHT_BY_EIGHT,
+    NINE_BY_SEVEN,
+    NINE_BY_NINE
+}
