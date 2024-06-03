f338f8c9229e36fd91424747a233e438f62f1411
xerox0213 <nasreddine021@gmail.com>
2024-06-03 20:30:08 +0200

    test(BoardTest): test de la méthode addToken
    Vérifie qu'elle lance une exception si le paramètre colIndex ne fait pas partie des limites du plateau de jeu

--- src/test/java/game/model/BoardTest.java
+++ src/test/java/game/model/BoardTest.java
@@ -1,7 +1,9 @@
 package game.model;
 
-import org.junit.jupiter.api.BeforeAll;
+import game.exception.ConnectFourError;
+import game.exception.ConnectFourException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,5 +14,13 @@ class BoardTest {
     @BeforeEach
     void setUp() {
         board = new Board(BoardSize.EIGHT_BY_SEVEN);
+    }
+
+    @Test
+    public void testAddTokenShouldThrowExceptionForOutsideBoard() {
+        int colIndex = 1000000;
+        Token token = Token.RED;
+        ConnectFourException e = assertThrows(ConnectFourException.class, () -> board.addToken(colIndex, token));
+        assertEquals(e.getConnectFourError(), ConnectFourError.OUTSIDE_BOARD);
     }
 }
