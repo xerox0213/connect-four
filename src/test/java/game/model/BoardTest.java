090c5eb5347cf2fde4631e75302a94f3420443a5
xerox0213 <nasreddine021@gmail.com>
2024-06-08 15:33:13 +0200

    feat(BoardTest): création d'un mock de l'interface Observer

--- src/test/java/game/model/BoardTest.java
+++ src/test/java/game/model/BoardTest.java
@@ -2,15 +2,23 @@ package game.model;
 
 import game.exception.ConnectFourError;
 import game.exception.ConnectFourException;
+import game.oo.Observer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class BoardTest {
 
+    @Mock
+    private Observer observer;
     private Token[][] tokens;
 
     void fillColumn(int colIndex, int numberTokens, Token token) {
