1c9c31be2a7bcb491b5f3dd44ce8de80426be6f5
xerox0213 <nasreddine021@gmail.com>
2024-06-15 12:37:03 +0200

    feat: création de la classe Player

---
+++ src/main/java/game/model/Player.java
@@ -0,0 +1,27 @@
+package game.model;
+
+import game.oo.Observer;
+
+import java.util.Set;
+
+public class Player {
+    private final String name;
+    private final Token token;
+    private final Time time;
+    private final Set<Observer> observers;
+
+    public Player(String name, Token token, Time time, Set<Observer> observers) {
+        this.name = name;
+        this.token = token;
+        this.time = time;
+        this.observers = observers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+}
