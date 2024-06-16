0212a679efc5aed23497a23672ad7fe9a2e3a704
xerox0213 <nasreddine021@gmail.com>
2024-06-16 14:03:24 +0200

    feat(LocalGame): implémentation de l'interface Game

--- src/main/java/game/model/LocalGame.java
+++ src/main/java/game/model/LocalGame.java
@@ -1,6 +1,8 @@
 package game.model;
 
-public class LocalGame {
+import game.exception.ConnectFourException;
+
+public class LocalGame implements Game {
     private final Board board;
     private final PlayerManager playerManager;
     private final RoundTimer roundTimer;
@@ -9,5 +11,25 @@ public class LocalGame {
         this.board = board;
         this.playerManager = playerManager;
         this.roundTimer = roundTimer;
+    }
+
+    @Override
+    public void play(int columnIndex) {
+        try {
+            roundTimer.stop();
+            Token currPlayerToken = playerManager.getCurrPlayerToken();
+            boolean isWinningMove = board.addToken(columnIndex, currPlayerToken);
+            boolean isBoardFull = board.isBoardFull();
+            if (isWinningMove) {
+                playerManager.declareCurrPlayerWinner();
+            } else if (isBoardFull) {
+                playerManager.declareGameDraw();
+            } else {
+                playerManager.nextPlayer();
+                roundTimer.start();
+            }
+        } catch (ConnectFourException e) {
+            roundTimer.start();
+        }
     }
 }
