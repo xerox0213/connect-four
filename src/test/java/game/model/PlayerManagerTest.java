d9f89c5451581471dbeb34b789e3ed9299497264
xerox0213 <nasreddine021@gmail.com>
2024-06-16 12:02:39 +0200

    feat: création de la classe de test PlayerManagerTest

---
+++ src/test/java/game/model/PlayerManagerTest.java
@@ -0,0 +1,25 @@
+package game.model;
+
+import game.exception.ConnectFourError;
+import game.exception.ConnectFourException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PlayerManagerTest {
+    @Mock
+    Player link;
+    @Mock
+    Player zelda;
+
+    @BeforeEach
+    void setUp() throws ConnectFourException {
+        ConnectFourException e = new ConnectFourException(ConnectFourError.NO_TIME_LEFT);
+        Mockito.lenient().doThrow(e).when(zelda).reduceTime(1000);
+    }
+}
