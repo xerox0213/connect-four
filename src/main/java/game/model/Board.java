476f57874c01159f83f0f158f7f91b20863952fc
xerox0213 <nasreddine021@gmail.com>
2024-06-17 20:04:55 +0200

    refactor: encapsulation des algorithmes de la classe Board
    Permet à l'ordinateur d'en bénéficier lors de son tour afin de ne pas répéter le même code

--- src/main/java/game/model/Board.java
+++ src/main/java/game/model/Board.java
@@ -25,25 +25,20 @@ public class Board implements Observable {
     }
 
     public boolean addToken(int colIndex, Token token) throws ConnectFourException {
-        if (isOutsideBoard(colIndex)) throw new ConnectFourException(ConnectFourError.OUTSIDE_BOARD);
+        if (BoardAlgorithm.isOutsideBoard(tokens, colIndex)) throw new ConnectFourException(ConnectFourError.OUTSIDE_BOARD);
 
-        int rowIndex = getFreeRowIndex(colIndex);
+        int rowIndex = BoardAlgorithm.getFreeRowIndex(tokens, colIndex);
         if (rowIndex == -1) throw new ConnectFourException(ConnectFourError.COLUMN_FILLED);
 
         tokens[colIndex][rowIndex] = token;
         Token[][] copyTokens = getCopyTokens();
 
         notifyObservers(ConnectFourEvent.BOARD_UPDATED, copyTokens);
-        return isWinningMove(colIndex, rowIndex, token);
+        return BoardAlgorithm.isWinningMove(tokens, colIndex, rowIndex, token);
     }
 
     public boolean isBoardFull() {
-        int colIndex = 0;
-        while (colIndex < cols) {
-            if (getFreeRowIndex(colIndex) != -1) return false;
-            colIndex++;
-        }
-        return true;
+        return BoardAlgorithm.isBoardFull(tokens);
     }
 
     public Token[][] getCopyTokens() {
@@ -52,46 +47,6 @@ public class Board implements Observable {
             System.arraycopy(tokens[colIndex], 0, copyTokens[colIndex], 0, tokens[0].length);
         }
         return copyTokens;
-    }
-
-    private boolean isOutsideBoard(int colIndex) {
-        return colIndex < 0 || colIndex >= cols;
-    }
-
-    private boolean isOutsideBoard(int colIndex, int rowIndex) {
-        return isOutsideBoard(colIndex) || (rowIndex < 0 || rowIndex >= rows);
-    }
-
-    private int getFreeRowIndex(int colIndex) {
-        int rowIndex = rows - 1;
-        while (rowIndex > -1) {
-            if (tokens[colIndex][rowIndex] == null) return rowIndex;
-            rowIndex--;
-        }
-        return -1;
-    }
-
-    private boolean isWinningMove(int tokenColIndex, int tokenRowIndex, Token playedToken) {
-        Directions[] directions = Directions.values();
-        for (Directions direction : directions) {
-            int numberTokensAligned = 1;
-            for (int i = 0; i < 2; i++) {
-                int dx = i == 0 ? direction.getDx() : direction.getOppositeDx();
-                int dy = i == 0 ? direction.getDy() : direction.getOppositeDy();
-                int colIndex = tokenColIndex + dx;
-                int rowIndex = tokenRowIndex + dy;
-                while (!isOutsideBoard(colIndex, rowIndex)) {
-                    Token token = tokens[colIndex][rowIndex];
-                    if (token == playedToken) {
-                        numberTokensAligned++;
-                        if (numberTokensAligned >= 4) return true;
-                    }
-                    colIndex += dx;
-                    rowIndex += dy;
-                }
-            }
-        }
-        return false;
     }
 
     @Override
---
+++ src/main/java/game/model/BoardAlgorithm.java
@@ -0,0 +1,53 @@
+package game.model;
+
+public class BoardAlgorithm {
+
+    public static boolean isBoardFull(Token[][] tokens){
+        int colIndex = 0;
+        while (colIndex < tokens.length) {
+            if (getFreeRowIndex(tokens, colIndex) != -1) return false;
+            colIndex++;
+        }
+        return true;
+    }
+
+    public static boolean isOutsideBoard(Token[][] tokens, int colIndex) {
+        return colIndex < 0 || colIndex >= tokens.length;
+    }
+
+    public static boolean isOutsideBoard(Token[][] tokens, int colIndex, int rowIndex) {
+        return isOutsideBoard(tokens, colIndex) || (rowIndex < 0 || rowIndex >= tokens[0].length);
+    }
+
+    public static int getFreeRowIndex(Token[][] tokens, int colIndex) {
+        int rowIndex = tokens[0].length - 1;
+        while (rowIndex > -1) {
+            if (tokens[colIndex][rowIndex] == null) return rowIndex;
+            rowIndex--;
+        }
+        return -1;
+    }
+
+    public static boolean isWinningMove(Token[][] tokens, int tokenColIndex, int tokenRowIndex, Token playedToken) {
+        Directions[] directions = Directions.values();
+        for (Directions direction : directions) {
+            int numberTokensAligned = 1;
+            for (int i = 0; i < 2; i++) {
+                int dx = i == 0 ? direction.getDx() : direction.getOppositeDx();
+                int dy = i == 0 ? direction.getDy() : direction.getOppositeDy();
+                int colIndex = tokenColIndex + dx;
+                int rowIndex = tokenRowIndex + dy;
+                while (!isOutsideBoard(tokens, colIndex, rowIndex)) {
+                    Token token = tokens[colIndex][rowIndex];
+                    if (token == playedToken) {
+                        numberTokensAligned++;
+                        if (numberTokensAligned >= 4) return true;
+                    }
+                    colIndex += dx;
+                    rowIndex += dy;
+                }
+            }
+        }
+        return false;
+    }
+}
