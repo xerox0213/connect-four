e0fc5b9b7268811b7ea62f04f40940999c81fb3e
xerox0213 <nasreddine021@gmail.com>
2024-06-17 21:00:26 +0200

    test(BoardAlgorithmTest): test des méthodes isOutsideBoard

--- src/test/java/game/model/BoardAlgorithmTest.java
+++ src/test/java/game/model/BoardAlgorithmTest.java
@@ -19,4 +19,31 @@ class BoardAlgorithmTest {
         boolean isBoardFull = BoardAlgorithm.isBoardFull(tokens);
         assertFalse(isBoardFull);
     }
+
+    @Test
+    void testIsOutsideBoardWithJustColumnIndexShouldReturnTrue() {
+        Token[][] tokens = {{Token.BLUE, null}, {Token.BLUE, Token.RED}};
+        assertTrue(BoardAlgorithm.isOutsideBoard(tokens, -1));
+        assertTrue(BoardAlgorithm.isOutsideBoard(tokens, tokens.length));
+    }
+
+    @Test
+    void testIsOutsideBoardWithJustColumnIndexShouldReturnFalse() {
+        Token[][] tokens = {{Token.BLUE, null}, {Token.BLUE, Token.RED}};
+        assertFalse(BoardAlgorithm.isOutsideBoard(tokens, 0));
+        assertFalse(BoardAlgorithm.isOutsideBoard(tokens, 1));
+    }
+
+    @Test
+    void testIsOutsideBoardWithColumnIndexAndRowIndexShouldReturnTrue() {
+        Token[][] tokens = {{Token.BLUE, null}, {Token.BLUE, Token.RED}};
+        assertTrue(BoardAlgorithm.isOutsideBoard(tokens, tokens.length, -1));
+        assertTrue(BoardAlgorithm.isOutsideBoard(tokens, tokens.length, tokens[0].length));
+    }
+
+    @Test
+    void testIsOutsideBoardWithColumnIndexAndRowIndexShouldReturnFalse() {
+        Token[][] tokens = {{Token.BLUE, null}, {Token.BLUE, Token.RED}};
+        assertFalse(BoardAlgorithm.isOutsideBoard(tokens, 0, 0));
+    }
 }
