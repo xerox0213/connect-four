4ec2d92b762828f97c4ee60ae50eff88ff868248
xerox0213 <nasreddine021@gmail.com>
2024-06-17 21:01:00 +0200

    test(BoardAlgorithmTest): test de la méthode getFreeRowIndex

--- src/test/java/game/model/BoardAlgorithmTest.java
+++ src/test/java/game/model/BoardAlgorithmTest.java
@@ -46,4 +46,20 @@ class BoardAlgorithmTest {
         Token[][] tokens = {{Token.BLUE, null}, {Token.BLUE, Token.RED}};
         assertFalse(BoardAlgorithm.isOutsideBoard(tokens, 0, 0));
     }
+
+    @Test
+    void testGetFreeRowIndexShouldNotHaveFreeRowIndex() {
+        Token[][] tokens = {{Token.BLUE, Token.RED}, {Token.BLUE, Token.RED}};
+        int expected = -1;
+        int rowIndex = BoardAlgorithm.getFreeRowIndex(tokens, 0);
+        assertEquals(expected, rowIndex);
+    }
+
+    @Test
+    void testGetFreeRowIndexShouldHaveFreeRowIndex() {
+        Token[][] tokens = {{Token.BLUE, Token.RED}, {null, Token.RED}};
+        int expected = 0;
+        int rowIndex = BoardAlgorithm.getFreeRowIndex(tokens, 1);
+        assertEquals(expected, rowIndex);
+    }
 }
