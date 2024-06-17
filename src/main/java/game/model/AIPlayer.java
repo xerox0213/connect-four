93f7edfe2c6b1e4b4e55419a20aae4ee8d9944f5
xerox0213 <nasreddine021@gmail.com>
2024-06-17 21:02:44 +0200

    feat: création de la classe AIPlayer

---
+++ src/main/java/game/model/AIPlayer.java
@@ -0,0 +1,19 @@
+package game.model;
+
+import game.oo.ConnectFourEvent;
+import game.oo.Observer;
+
+import java.util.Set;
+
+public class AIPlayer extends Player {
+    private final AIStrategy aiStrategy;
+    private final LocalGame localGame;
+    private Token[][] copyTokens;
+
+    public AIPlayer(String name, Token token, Time time, Set<Observer> observers, AIStrategy aiStrategy, LocalGame localGame, Token[][] copyTokens) {
+        super(name, token, time, observers);
+        this.aiStrategy = aiStrategy;
+        this.localGame = localGame;
+        this.copyTokens = copyTokens;
+    }
+}
