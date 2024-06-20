d4a2e1ffb2124f5a5da0aea48797d2360c9689ce
xerox0213 <nasreddine021@gmail.com>
2024-06-20 21:21:34 +0200

    feat(ConnectFourPresenter): implémentation de l'inteface Observer

--- src/main/java/game/presenter/ConnectFourPresenter.java
+++ src/main/java/game/presenter/ConnectFourPresenter.java
@@ -2,8 +2,10 @@ package game.presenter;
 
 import game.model.Game;
 import game.model.GameRoom;
+import game.oo.ConnectFourEvent;
+import game.oo.Observer;
 
-public class ConnectFourPresenter {
+public class ConnectFourPresenter implements Observer {
     private final GameRoom gameRoom;
     private Game game;
 
@@ -13,5 +15,10 @@ public class ConnectFourPresenter {
 
     public void setPlayerName(String playerName) {
         gameRoom.setMyPlayerName(playerName);
+    }
+
+    @Override
+    public void update(ConnectFourEvent e, Object data) {
+
     }
 }
