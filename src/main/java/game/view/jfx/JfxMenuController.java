c871c3e2f17c1437dba071b88d336675ff5b5ad1
xerox0213 <nasreddine021@gmail.com>
2024-06-20 21:37:22 +0200

    feat: création de la classe JfxMenuController

---
+++ src/main/java/game/view/jfx/JfxMenuController.java
@@ -0,0 +1,17 @@
+package game.view.jfx;
+
+import game.presenter.ConnectFourPresenter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class JfxMenuController extends Showable {
+    private final ConnectFourPresenter connectFourPresenter;
+    @FXML
+    private Button playAgainstComputerBtn;
+
+    public JfxMenuController(ShowStrategy showStrategy, ConnectFourPresenter connectFourPresenter) {
+        super(showStrategy);
+        this.connectFourPresenter = connectFourPresenter;
+    }
+
+}
