915748a0b345ce6b85fb8f675730e086b7ecd763
xerox0213 <nasreddine021@gmail.com>
2024-06-20 21:14:53 +0200

    feat: création de la classe JfxHomeController

---
+++ src/main/java/game/view/jfx/JfxHomeController.java
@@ -0,0 +1,19 @@
+package game.view.jfx;
+
+import game.presenter.ConnectFourPresenter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class JfxHomeController extends Showable {
+    private final ConnectFourPresenter connectFourPresenter;
+    @FXML
+    private Label playerNameLabel;
+    @FXML
+    private Button confirmPlayerNameBtn;
+
+    public JfxHomeController(ShowStrategy showStrategy, ConnectFourPresenter connectFourPresenter) {
+        super(showStrategy);
+        this.connectFourPresenter = connectFourPresenter;
+    }
+}
