203e116982a2d36e4e9e436b5e57c8f299dd69ff
xerox0213 <nasreddine021@gmail.com>
2024-06-20 21:27:35 +0200

    feat(ConnectFourPresenter): liaison du presenter avec la facade de vue

--- src/main/java/game/presenter/ConnectFourPresenter.java
+++ src/main/java/game/presenter/ConnectFourPresenter.java
@@ -4,10 +4,12 @@ import game.model.Game;
 import game.model.GameRoom;
 import game.oo.ConnectFourEvent;
 import game.oo.Observer;
+import game.view.ConnectFourView;
 
 public class ConnectFourPresenter implements Observer {
     private final GameRoom gameRoom;
     private Game game;
+    private ConnectFourView connectFourView;
 
     public ConnectFourPresenter(GameRoom gameRoom) {
         this.gameRoom = gameRoom;
