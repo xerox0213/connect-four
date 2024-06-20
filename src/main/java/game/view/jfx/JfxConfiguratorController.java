78f166548fdd88ce1f1481bad0946273b7d66ccf
xerox0213 <nasreddine021@gmail.com>
2024-06-20 21:47:57 +0200

    feat: création de la classe JfxConfiguratorController

---
+++ src/main/java/game/view/jfx/JfxConfiguratorController.java
@@ -0,0 +1,26 @@
+package game.view.jfx;
+
+import game.model.BoardSize;
+import game.model.FirstPlayer;
+import game.model.PlayerTime;
+import game.model.RoundTime;
+import game.presenter.ConnectFourPresenter;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class JfxConfiguratorController extends Showable {
+    private final ConnectFourPresenter connectFourPresenter;
+    @FXML
+    private ComboBox<BoardSize> boardSizeComboBox;
+    @FXML
+    private ComboBox<RoundTime> lapTimeComboBox;
+    @FXML
+    private ComboBox<PlayerTime> minutesPerPlayerComboBox;
+    @FXML
+    private ComboBox<FirstPlayer> firstPlayerComboBox;
+
+    public JfxConfiguratorController(ShowStrategy showStrategy, ConnectFourPresenter connectFourPresenter) {
+        super(showStrategy);
+        this.connectFourPresenter = connectFourPresenter;
+    }
+}
