39fd681d6fc18c1f5acdc8b648a12f5835b359a8
xerox0213 <nasreddine021@gmail.com>
2024-06-24 22:19:07 +0200

    feat: création de la classe JfxEndCtrl

---
+++ src/main/java/game/view/jfx/JfxEndCtrl.java
@@ -0,0 +1,21 @@
+package game.view.jfx;
+
+import game.presenter.ConnectFourPresenter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class JfxEndCtrl extends Showable {
+    private final ConnectFourPresenter connectFourPresenter;
+    @FXML
+    private Label resultLabel;
+    @FXML
+    private Button newGameBtn;
+    @FXML
+    private Button quitBtn;
+
+    public JfxEndCtrl(ShowStrategy showStrategy, ConnectFourPresenter connectFourPresenter) {
+        super(showStrategy);
+        this.connectFourPresenter = connectFourPresenter;
+    }
+}
