28a47f75903c43b9bf317af15cb2d08234a6ee5f
xerox0213 <nasreddine021@gmail.com>
2024-06-24 18:55:48 +0200

    feat(BoardSize): ajout d'une représentation en chaine de caractère pour chaque instance

--- src/main/java/game/model/BoardSize.java
+++ src/main/java/game/model/BoardSize.java
@@ -4,18 +4,20 @@ package game.model;
  * Represents the possible board sizes for the Connect Four game.
  */
 public enum BoardSize {
-    SEVEN_BY_SIX(7, 6),
-    EIGHT_BY_SEVEN(8, 7),
-    EIGHT_BY_EIGHT(8, 8),
-    NINE_BY_SEVEN(9, 7),
-    NINE_BY_NINE(9, 9);
+    SEVEN_BY_SIX(7, 6, "7x6"),
+    EIGHT_BY_SEVEN(8, 7, "8x7"),
+    EIGHT_BY_EIGHT(8, 8, "8x8"),
+    NINE_BY_SEVEN(9, 7, "9x7"),
+    NINE_BY_NINE(9, 9, "9x9");
 
+    private final String representation;
     private final int cols;
     private final int rows;
 
-    BoardSize(int cols, int rows) {
+    BoardSize(int cols, int rows, String representation) {
         this.cols = cols;
         this.rows = rows;
+        this.representation = representation;
     }
 
     public int getCols() {
@@ -24,5 +26,10 @@ public enum BoardSize {
 
     public int getRows() {
         return rows;
+    }
+
+    @Override
+    public String toString() {
+        return representation;
     }
 }
