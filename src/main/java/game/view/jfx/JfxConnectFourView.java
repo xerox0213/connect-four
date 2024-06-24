f8fec81726ffe1b561eed0452ea022e1ab841684
xerox0213 <nasreddine021@gmail.com>
2024-06-24 11:41:53 +0200

    feat(JfxConnectFourView): implémentation de l'interface ConnectFourView

--- src/main/java/game/view/jfx/JfxConnectFourView.java
+++ src/main/java/game/view/jfx/JfxConnectFourView.java
@@ -1,6 +1,10 @@
 package game.view.jfx;
 
-public class JfxConnectFourView {
+import game.dto.GameDto;
+import game.model.Token;
+import game.view.ConnectFourView;
+
+public class JfxConnectFourView implements ConnectFourView {
     private final JfxMenuCtrl jfxMenuCtrl;
     private final JfxConfigCtrl jfxConfigCtrl;
     private final JfxGameCtrl jfxGameCtrl;
@@ -9,5 +13,51 @@ public class JfxConnectFourView {
         this.jfxMenuCtrl = JfxMenuCtrl;
         this.jfxConfigCtrl = jfxConfigCtrl;
         this.jfxGameCtrl = jfxGameCtrl;
+    }
+
+    @Override
+    public void showMenu() {
+        jfxMenuCtrl.show();
+    }
+
+    @Override
+    public void showConfigurator() {
+        jfxConfigCtrl.show();
+    }
+
+    @Override
+    public void showGame(GameDto gameDto) {
+        jfxGameCtrl.initGame(gameDto);
+        jfxGameCtrl.show();
+    }
+
+    @Override
+    public void showEnd() {
+
+    }
+
+    @Override
+    public void showError(String error) {
+        System.out.println(error);
+    }
+
+    @Override
+    public void updateBoard(Token token, int columnIndex, int rowIndex) {
+        jfxGameCtrl.updateBoard(token, columnIndex, rowIndex);
+    }
+
+    @Override
+    public void updateRoundTime(long millis) {
+        jfxGameCtrl.updateRoundTime(millis);
+    }
+
+    @Override
+    public void updatePlayerTime(long millis, boolean isMine) {
+        jfxGameCtrl.updatePlayerTime(millis, isMine);
+    }
+
+    @Override
+    public void updatePlayerTurn(boolean isMyTurn) {
+        jfxGameCtrl.updatePlayerTurn(isMyTurn);
     }
 }
